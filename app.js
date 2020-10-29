97193eda86319f956d59403bf1c28cb7ad77b5ab
BMO <ratcha.keunma@gmail.com>
2020-10-29 21:32:23 +0700

    fix app

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
-const bodyParser = require('body-parser')
-const cors = require('cors');
+// const bodyParser = require('body-parser')
+// const cors = require('cors');
 // const fileUpload = require('express-fileupload');
 // const morgan = require('morgan');
 // const _ = require('lodash');
