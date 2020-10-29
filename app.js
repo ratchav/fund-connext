db52ed4aa8b7f2740ec576b6eb51338388a060ed
BMO <ratcha.keunma@gmail.com>
2020-10-29 21:33:23 +0700

    change const to var

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const express = require('express')
-const app = express()
+var express = require('express')
+var app = express()
 // const bodyParser = require('body-parser')
 // const cors = require('cors');
 // const fileUpload = require('express-fileupload');
